1c8252cdccde84008c906eb5180f7d7a378b3a18
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2018-11-29 18:08:16 +0100

    Dodanie AutoFac

--- WPF.DependencyInjection/ViewModels/ViewModelLocator.cs
+++ WPF.DependencyInjection/ViewModels/ViewModelLocator.cs
@@ -1,4 +1,6 @@
-﻿using CommonServiceLocator;
+﻿using Autofac;
+using Autofac.Extras.CommonServiceLocator;
+using CommonServiceLocator;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,36 +19,71 @@ namespace WPF.DependencyInjection.ViewModels
     public class ViewModelLocator
     {
 
-        private readonly UnityContainer container;
+        // private readonly UnityContainer container;
+        // private readonly IContainer container;
+
         private readonly LifetimeManager _mainWindowLifetimeManager = new ContainerControlledLifetimeManager();
         private readonly LifetimeManager _databaseLifetimeManager = new TransientLifetimeManager();
 
         public ViewModelLocator()
         {
-            container = new UnityContainer();            
+            UseUnity();
+
+            // UseAutoFac();
+
+        }
+
+        private void UseUnity()
+        {
+            var container = new UnityContainer();
             container.RegisterType<CustomersViewModel>(_mainWindowLifetimeManager);
             container.RegisterType<ICustomersService, CustomersService>();
             container.RegisterType<IFrameNavigationService, FrameNavigationService>();
+
+            // Set the service locator to an UnityServiceLocator.
+            ServiceLocator.SetLocatorProvider(() => new UnityServiceLocator(container));
 
             //container.RegisterTypes(
             //       AllClasses.FromLoadedAssemblies(),
             //       WithMappings.FromMatchingInterface,
             //       WithName.Default);
+        }
 
-            // ServiceLocator.SetLocatorProvider(() => new UnityServiceLocator(container));
+        private static void UseAutoFac()
+        {
+            var builder = new ContainerBuilder();
 
+            builder.RegisterType<FrameNavigationService>().As<IFrameNavigationService>();
+            builder.RegisterType<CustomersService>().As<ICustomersService>();
+
+            builder.RegisterType<ShellViewModel>();
+            builder.RegisterType<Page1ViewModel>();
+            builder.RegisterType<CustomersViewModel>();
+
+            // Perform registrations and build the container.
+            var container = builder.Build();
+
+            // Set the service locator to an AutofacServiceLocator.
+            // PM> Install-Package Autofac.Extras.CommonServiceLocator
+            ServiceLocator.SetLocatorProvider(() => new AutofacServiceLocator(container));
         }
 
 
         // ServiceLocator jest abstrakcją do wstrzykiwania. 
         // Izoluje nas od konkretnej implementacji DI
 
-        // Wstrzykiwanie poprzez ServiceLocator
+        // Wstrzykiwanie pośrednie poprzez ServiceLocator
         public ShellViewModel ShellViewModel => ServiceLocator.Current.GetInstance<ShellViewModel>();
         public CustomersViewModel CustomersViewModel => ServiceLocator.Current.GetInstance<CustomersViewModel>();
         public Page1ViewModel Page1ViewModel => ServiceLocator.Current.GetInstance<Page1ViewModel>();
 
-        // Wstrzykiwanie za pomocą Unity
+        // Wstrzykiwanie bezpośrednie za pomocą Unity
+        //public ShellViewModel ShellViewModel => container.Resolve<ShellViewModel>();
+        //public CustomersViewModel CustomersViewModel => container.Resolve<CustomersViewModel>();
+        //public Page1ViewModel Page1ViewModel => container.Resolve<Page1ViewModel>();
+
+
+        // Wstrzykiwanie bezpośrednie za pomocą AutoFac
         //public ShellViewModel ShellViewModel => container.Resolve<ShellViewModel>();
         //public CustomersViewModel CustomersViewModel => container.Resolve<CustomersViewModel>();
         //public Page1ViewModel Page1ViewModel => container.Resolve<Page1ViewModel>();
